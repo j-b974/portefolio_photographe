d9fc05a17420b4082d8b7fdd682738b576a14a0b
jb974 <bertil974@hotmail.fr>
2023-05-15 13:44:50 +0200

    home,categorie implément

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,12 @@
-console.log("connecté!!!!")
+console.log("connecté!!!!");
+const btnToggle = document.querySelector('.toggleBtn');
+const icon = document.querySelector('.toggleBtn i ');
+const menuDrop = document.querySelector('.dropdownMenu');
+
+btnToggle.addEventListener('click',function(){
+    menuDrop.classList.toggle('open');
+
+    let isOpen = menuDrop.classList.contains('open');
+
+    icon.className = isOpen ? 'fa-solid fa-xmark':'fa-solid fa-bars';
+})
