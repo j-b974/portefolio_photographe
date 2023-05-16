0b2e5f9d838fd2c5dc553c7623c41bdebdae35c0
jb974 <bertil974@hotmail.fr>
2023-05-16 07:01:24 +0200

    categorie swiper

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -10,3 +10,53 @@ btnToggle.addEventListener('click',function(){
 
     icon.className = isOpen ? 'fa-solid fa-xmark':'fa-solid fa-bars';
 })
+
+
+// =============   Swiper ==========
+
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  });
+
+  const lstBtnMenu = document.querySelectorAll(".lstTheme li a");
+  const titreCat = document.querySelector('.titreCategorie span');
+  const SwiperLoad = document.querySelector('#swiperLoader');
+
+  lstBtnMenu.forEach((element)=>{
+    element.addEventListener('click', function(event){
+        event.preventDefault();
+        let categorie = element.textContent;
+        titreCat.textContent = categorie;
+
+        loadListImg(categorie);
+
+    })
+  })
+
+  function loadListImg(cat){
+    
+
+  }
+
+  function ShowSwiper(lstImage){
+
+    if(!lstImage){return;}
+    SwiperLoad.innerHTML = '';
+    lstImage.forEach(function(path){
+        SwiperLoad.innerHTML += `<div class="swiper-slide"><img src="${path}" /></div>`;
+    })
+  }
+
+
+
+//  =====================================
