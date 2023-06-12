748797b31b06d4d45909bb562b7b598b266661b0
jb974 <bertil974@hotmail.fr>
2023-06-12 09:50:39 +0200

    ajout fichier externe

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -116,8 +116,31 @@ btnToggle.addEventListener('click',function(){
             str += `<div class="swiper-slide"><img src="${path}" /></div>`;
         })
         SwiperLoad.innerHTML = str;
-        creatSwiper();
+        // creatSwiper();
     }
   }
 
 //  =====================================
+
+
+// ================= netlify contact ajax ========
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+  
+  document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
+  
